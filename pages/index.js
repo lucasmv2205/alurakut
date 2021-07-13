b22340f72f099343c366dad65193cb10a96e7b2a
lucasmv2205 <lmartins2205@gmail.com>
2021-07-13 18:19:01 -0300

    create form to create a new comunity

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,66 @@
-import  { MainGrid }  from '../src/components/MainGrid';
-import  { Box }  from '../src/components/Box';
+import { MainGrid } from '../src/components/MainGrid';
+import { Box } from '../src/components/Box';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 import { ComunitiesList } from '../src/components/ComunitiesList';
 import { FriendsList } from '../src/components/FriendsList';
-import { AlurakutMenu, OrkutNostalgicIconSet  } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { useCallback, useState } from 'react';
 
 function ProfileSidebar(props) {
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
 
 export default function Home() {
   const githubUser = 'lucasmv2205';
+  const [comunities, setComunities] = useState([
+    { id: "2021-07-09T20:53:51.130Z", name: "Postgres", logo: "https://www.cyclonis.com/images/2018/10/1_7AOhGDnRL2eyJMUidCHZEA-765x383.jpg" },
+    { id: "2021-07-10T20:53:51.130Z", name: "Morre Praga", logo: "https://s2.glbimg.com/6C8iXLc146uY7UcX1kbDiprbD3k=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2021/5/v/YTfYLvSdm55eJTuZxCNg/memes-phoenix-force-mundial-free-fire-ffws-2021.jpeg" },
+    { id: "2021-07-11T20:53:51.130Z", name: "Vercel", logo: "https://res.cloudinary.com/practicaldev/image/fetch/s--UajhAYy4--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/emsbo1jy8jh91vvohwrj.jpeg" },
+    { id: "2021-07-12T20:53:51.130Z", name: "ReactJS", logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" }
+  ]);
+
+
+  const handleCreateComunity = useCallback((event) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    console.log(data.get('title'));
+    console.log(data.get('image'));
+
+    const comunity = {
+      id: new Date().toISOString(),
+      name: data.get('title'),
+      logo: data.get('image'),
+    }
+    const updatedComunities = [...comunities, comunity];
+    console.log(updatedComunities);
+    setComunities(updatedComunities);
+  }, [])
+
+
 
   return (
     <>
-      <AlurakutMenu />
+      <AlurakutMenu githubUser={githubUser} />
       <MainGrid>
-        <div className="profileArea" style={{gridArea: 'profileArea'}}>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
           <ProfileSidebar githubUser={githubUser} />
         </div>
 
-        <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
           <Box>
             <h1 className="title">
               Bem vindo(a)
@@ -32,15 +68,40 @@ export default function Home() {
 
             <OrkutNostalgicIconSet />
           </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer?</h2>
+            <form onSubmit={handleCreateComunity}>
+              <div>
+                <input
+                  placeholder="Qual vai ser o nome da sua comunidade"
+                  name="title"
+                  aria-label="Qual vai ser o nome da sua comunidade"
+                  type="text"
+                />
+              </div>
+              <div>
+                <input
+                  placeholder="Coloque uma url para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma url para usarmos de capa"
+                />
+              </div>
+
+              <button type="submit">
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
 
-        <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
           <ProfileRelationsBoxWrapper>
             <FriendsList githubUser={githubUser} />
           </ProfileRelationsBoxWrapper>
 
           <ProfileRelationsBoxWrapper>
-            <ComunitiesList />
+            <ComunitiesList comunities={comunities} />
           </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
--- src/components/FriendsList/index.js
+++ src/components/FriendsList/index.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 
-export function FriendsList(props){
+export function FriendsList(props) {
     const [followers, setFollowers] = useState([]);
 
     useEffect(() => {
         fetch(`https://api.github.com/users/${props.githubUser}/followers`)
-        .then(response => response.json())
-        .then(data => setFollowers(data))
+            .then(response => response.json())
+            .then(data => setFollowers(data))
     }, []);
 
-    return(
+    return (
         <>
             <h2 className="smallTitle">
                 Amigos ({followers.length})
@@ -17,14 +17,14 @@ export function FriendsList(props){
 
             <ul>
                 {followers.map((follower) => {
-                return(
-                    <li>
-                    <a href={`/users/${follower.login}`} key={follower.id}>
-                        <img src={`https://github.com/${follower.login}.png`} />
-                        <span>{follower.login}</span>
-                    </a>
-                    </li>
-                );
+                    return (
+                        <li key={follower.id}>
+                            <a href={`https://github.com/${follower.login}`}>
+                                <img src={`https://github.com/${follower.login}.png`} />
+                                <span>{follower.login}</span>
+                            </a>
+                        </li>
+                    );
                 })}
             </ul>
         </>
